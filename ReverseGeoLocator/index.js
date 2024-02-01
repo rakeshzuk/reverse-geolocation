9d53c8db27cab664c24f194db5faf7b27e42f6f7
Rakesh Sharma <rakesh.sharma@in.seamovation.com>
2024-02-02 00:17:59 +0530

    efficiant

--- ReverseGeoLocator/index.js
+++ ReverseGeoLocator/index.js
@@ -1,11 +1,10 @@
-
 if ("geolocation" in navigator) {
-    /* geolocation is available */
-    console.log("geolocation is available");
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log(position.coords.latitude, position.coords.longitude);
-      window.location.href = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
-    });
+  /* geolocation is available */
+  console.log("geolocation is available");
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position.coords.latitude, position.coords.longitude);
+    window.location.href = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+  });
 } else {
   /* geolocation IS NOT available */
 }
