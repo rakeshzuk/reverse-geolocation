06aeacad8f7e3692e298a9b1ac90b0d208302528
Rakesh Sharma <rakesh.sharma@in.seamovation.com>
2024-02-02 00:16:30 +0530

    geolocation method

--- ReverseGeoLocator/index.js
+++ ReverseGeoLocator/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+
+if ("geolocation" in navigator) {
+    /* geolocation is available */
+    console.log("geolocation is available");
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      window.location.href = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+    });
+} else {
+  /* geolocation IS NOT available */
+}
